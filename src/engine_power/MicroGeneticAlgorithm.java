2427bd14b720fcf7d8aa6fcfcffd4c8209f8e55d
recaky <115483491+recaky@users.noreply.github.com>
2023-06-09 21:11:09 +0200

    Update MicroGeneticAlgorithm.java

--- src/engine_power/MicroGeneticAlgorithm.java
+++ src/engine_power/MicroGeneticAlgorithm.java
@@ -1,23 +1,19 @@
-package engine_power ;
+package engine_power;
 import java.util.Random;
 
-public class MicroGeneticAlgorithm {
+public class prva {
     private static final double PI = Math.PI;
-    private static final int POPULATION_SIZE = 1000;
-    private static final int NUM_GENERATIONS = 2000;
-
-
-
-
-
+    private static final int POPULATION_SIZE = 100;
+    private static final int NUM_GENERATIONS = 100;
+    private static final int TOURNAMENT_SIZE = 5; // Tournament size for selection
 
     private static final int cylindersnumber = 4;
 
-    private static final double MIN_VALVE_MEP = 170.0; //psi
+    private static final double MIN_VALVE_MEP = 170.0; // psi
     private static final double MAX_VALVE_MEP = 280.0;
-    private static final double MIN_STROKE_LENGTH = 0.27;//ft
+    private static final double MIN_STROKE_LENGTH = 0.27; // ft
     private static final double MAX_STROKE_LENGTH = 0.3;
-    private static final double MIN_BORE = 2.9; //in
+    private static final double MIN_BORE = 2.9; // in
     private static final double MAX_BORE = 3.5;
     private static final double MIN_REVS = 3500;
     private static final double MAX_REVS = 4000;
@@ -41,37 +37,40 @@ public class MicroGeneticAlgorithm {
             for (int i = 0; i < POPULATION_SIZE; i++) {
                 double[] engineParams = population[i];
 
-                fitness[i] = (cylindersnumber*engineParams[0]*engineParams[1]*(PI/4)*(Math.pow(engineParams[2],2))*(engineParams[3]))/(2*33000);
-
+                fitness[i] = calculateFitness(engineParams);
             }
 
             // Select parents for crossover
             double[][] parents = new double[POPULATION_SIZE][4];
+            parents[0] = tournamentSelection(population, fitness, TOURNAMENT_SIZE);
+            parents[1] = tournamentSelection(population, fitness, TOURNAMENT_SIZE);
+            double[] offspring = crossover (parents[0], parents[1]);
+            // replace worst
+            int worstIndex = 0;
+            double worstFitness = Double.MAX_VALUE;
             for (int i = 0; i < POPULATION_SIZE; i++) {
-                int parent1Index = selectParent(fitness);
-                int parent2Index = selectParent(fitness);
-                parents[i] = crossover(population[parent1Index], population[parent2Index]);
+                if (fitness[i] < worstFitness) {
+                    worstIndex = i;
+                    worstFitness = fitness[i];
+                }
             }
-
-            // Replace old population with new offspring
-            population = parents;
+            population[worstIndex] = offspring;
 
 
+
+            // Output best fitness in the current generation
             double bestFitness = Double.NEGATIVE_INFINITY;
             double[] bestParams = null;
             for (int i = 0; i < POPULATION_SIZE; i++) {
                 double[] engineParams = population[i];
-
-                double fitnessik = (cylindersnumber*engineParams[0]*engineParams[1]*(PI/4)*(Math.pow(engineParams[2],2))*(engineParams[3]))/(2*33000);
+                double fitnessik = calculateFitness(engineParams);
 
                 if (fitnessik > bestFitness) {
                     bestFitness = fitnessik;
                     bestParams = engineParams;
                 }
             }
-            System.out.println("Currentgen: "+generation+" Power: " + (cylindersnumber*bestParams[0]*bestParams[1]*(PI/4)*(Math.pow(bestParams[2],2))*(bestParams[3]))/(2*33000) );
-
-
+            System.out.println(bestFitness + ",");
         }
 
         // Find best engine parameter set and display results
@@ -79,8 +78,7 @@ public class MicroGeneticAlgorithm {
         double[] bestParams = null;
         for (int i = 0; i < POPULATION_SIZE; i++) {
             double[] engineParams = population[i];
-
-            double fitness = (cylindersnumber*engineParams[0]*engineParams[1]*(PI/4)*(Math.pow(engineParams[2],2))*(engineParams[3]))/(2*33000);
+            double fitness = calculateFitness(engineParams);
 
             if (fitness > bestFitness) {
                 bestFitness = fitness;
@@ -93,26 +91,11 @@ public class MicroGeneticAlgorithm {
         System.out.println("Stroke: " + bestParams[1]);
         System.out.println("Bore: " + bestParams[2]);
         System.out.println("Revs: " + bestParams[3]);
-        System.out.println("Power: " + (cylindersnumber*bestParams[0]*bestParams[1]*(PI/4)*(Math.pow(bestParams[2],2))*(bestParams[3]))/(2*33000) );
+        System.out.println("Power: " + calculateFitness(bestParams));
 
     }
 
-    private static int selectParent(double[] fitness) {
-        double totalFitness = 0.0;
-        for (double f : fitness) {
-            totalFitness += f;
-        }
 
-        double rand = random.nextDouble() * totalFitness;
-        int index = 0;
-        while (rand > 0) {
-            rand -= fitness[index];
-            index++;
-        }
-        index--;
-
-        return index;
-    }
 
     private static double[] crossover(double[] parent1, double[] parent2) {
         double[] offspring = new double[4];
@@ -123,8 +106,35 @@ public class MicroGeneticAlgorithm {
         return offspring;
     }
 
+    private static double calculateFitness(double[] engineParams) {
+        return (cylindersnumber * engineParams[0] * engineParams[1] * (PI / 4) * (Math.pow(engineParams[2], 2))
+                * engineParams[3]) / (2 * 33000);
+    }
 
+    private static double[] tournamentSelection(double[][] population, double[] fitness, int tournamentSize) {
+        int[] tournamentIndices = new int[tournamentSize];
 
+        // Randomly select individuals for the tournament
+        for (int i = 0; i < tournamentSize; i++) {
+            tournamentIndices[i] = random.nextInt(population.length);
+        }
+
+        // Find the fittest individual in the tournament
+        int fittestIndex = tournamentIndices[0];
+        double fittestFitness = fitness[fittestIndex];
+        
+        for (int i = 1; i < tournamentSize; i++) {
+            int currentIndex = tournamentIndices[i];
+            double currentFitness = fitness[currentIndex];
+
+            if (currentFitness > fittestFitness) {
+                fittestIndex = currentIndex;
+                fittestFitness = currentFitness;
+            }
+        }
+
+        // Return the fittest individual
+        return population[fittestIndex];
+    }
 }
 
-            
